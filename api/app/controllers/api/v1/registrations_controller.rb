51e8f29b774c3957269e483800382b3e308b09df
e-nk <enocklagat747@gmail.com>
2024-09-20 12:22:22 +0300

    response updated

--- api/app/controllers/api/v1/registrations_controller.rb
+++ api/app/controllers/api/v1/registrations_controller.rb
@@ -7,19 +7,9 @@ module Api
       def create
         user = User.new(user_params)
         if user.save
-          render json: {
-            messages: "Signed Up Successfully",
-            is_success: true,
-            data: {
-              user: user
-            }
-          }, status: :ok
+          json_response "Signup successful", true, { user: user }, :ok
         else
-          render json: {
-            messages: "Something is wrong",
-            is_success: false,
-            data: {}
-          }, status: :unprocessable_entity
+          json_response "Something wrong", false, { user: user }, :unprocessable_entity
         end
       end
 
@@ -31,11 +21,7 @@ module Api
 
       def ensure_params_exist
         return if params[:user].present?
-        render json: {
-          messages: "Params are missing",
-          is_success: false,
-          data: {}
-        }, status: :bad_request
+        json_response "missing params", false, {}, :bad_request
       end
     end
   end
