1ac98acde0980057f3fdd9e51e5053ea749da3a8
e-nk <enocklagat747@gmail.com>
2024-09-20 12:08:43 +0300

    test registration successfull

--- api/app/controllers/api/v1/registrations_controller.rb
+++ api/app/controllers/api/v1/registrations_controller.rb
@@ -1,34 +1,42 @@
-class API::V1::RegistrationsController < Devise::RegistrationsController
-  before_action :ensure_params_exist, only: :create
-  # signup
-  def create
-    user = User.new user_params
-    if user.save
-      render json: {
-        messages: "Signed Up Successfully",
-        is_success: true,
-        data: {
-          user: user
-        } 
-       }, status: :ok
-    else
-      render json: {
-        messages: "Something is wrong",
-        is_success: false,
-        data: {}
-      }, status: :unprocessable_entity
+module Api
+  module V1
+    class RegistrationsController < Devise::RegistrationsController
+      before_action :ensure_params_exist, only: :create
+
+      # signup
+      def create
+        user = User.new(user_params)
+        if user.save
+          render json: {
+            messages: "Signed Up Successfully",
+            is_success: true,
+            data: {
+              user: user
+            }
+          }, status: :ok
+        else
+          render json: {
+            messages: "Something is wrong",
+            is_success: false,
+            data: {}
+          }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def user_params
+        params.require(:user).permit(:email, :password, :password_confirmation)
+      end
+
+      def ensure_params_exist
+        return if params[:user].present?
+        render json: {
+          messages: "Params are missing",
+          is_success: false,
+          data: {}
+        }, status: :bad_request
+      end
     end
   end
-    private
-    def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation)
-    end
-    def ensure_params_exist
-      return if params[:user].present?
-      render json: {
-        messages: "Params are missing",
-        is_success: false,
-        data: {}
-      }, status: :bad_request
-    end
 end
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      devise_scope :users do
+      devise_scope :user do
         post "sign_up", to: "registrations#create"
       end
     end
